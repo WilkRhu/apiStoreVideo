62f2d656637b13e09fd1cf6e14ffab6db21a4ede
WilkRhu <wilk.caetano@gmail.com>
2020-10-15 22:40:52 -0300

    feature: include config running app

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+module.exports = app;
